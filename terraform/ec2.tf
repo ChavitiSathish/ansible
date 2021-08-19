7c15cf8941e9f091eec13c129b7cfdefcc7d91fc
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-19 17:50:23 +0530

    Ansible

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,8 +7,8 @@ resource "aws_spot_instance_request" "cheap_worker" {
   wait_for_fulfillment      = true
   spot_type                 = "persistent"
   tags                      = {
-    Name                    = element(var.COMPONENTS, count.index)
-    #Name                    ="${element(var.COMPONENTS, count.index)}-${var.ENV}"
+    #Name                    = element(var.COMPONENTS, count.index)
+    Name                    ="${element(var.COMPONENTS, count.index)}-${var.ENV}"
   }
 }
 
@@ -16,8 +16,8 @@ resource "aws_ec2_tag" "name-tag" {
   count                     = local.LENGTH
   resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key                       = "Name"
-  value                     = element(var.COMPONENTS, count.index)
-  #value                     = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
+  #value                     = element(var.COMPONENTS, count.index)
+  value                     = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
 }
 
 resource "aws_route53_record" "records" {
@@ -38,5 +38,5 @@ locals {
 
 resource "local_file" "inventory-file" {
   content = "[FRONTEND]\n${aws_spot_instance_request.cheap_worker.*.private_ip[9]}\n[PAYMENT]\n${aws_spot_instance_request.cheap_worker.*.private_ip[8]}\n[SHIPPING]\n${aws_spot_instance_request.cheap_worker.*.private_ip[7]}\n[USER]\n${aws_spot_instance_request.cheap_worker.*.private_ip[6]}\n[CATALOGUE]\n${aws_spot_instance_request.cheap_worker.*.private_ip[5]}\n[CART]\n${aws_spot_instance_request.cheap_worker.*.private_ip[4]}\n[REDIS]\n${aws_spot_instance_request.cheap_worker.*.private_ip[3]}\n[RABBITMQ]\n${aws_spot_instance_request.cheap_worker.*.private_ip[2]}\n[MONGODB]\n${aws_spot_instance_request.cheap_worker.*.private_ip[1]}\n[MYSQL]\n${aws_spot_instance_request.cheap_worker.*.private_ip[0]}"
-  filename = "/tmp/inv-roboshop"
+  filename = "/tmp/inv-roboshop-${var.ENV}"
 }
