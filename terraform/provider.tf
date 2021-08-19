7ef72ce096f1a2bfdae9267a5f04a010dfdaa29b
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-19 18:10:28 +0530

    Ansible

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#for specific environements
 terraform {
   backend "s3" { }
 }
+
+
+#for Common environements
+
+#terraform {
+#  backend "s3" {
+#  bucket            = "chtfbucket"
+#    key               = "roboshop/ansible/dev/terraform.tfstate"
+#    region            = "us-east-1"
+#    dynamodb_table    = "terraform"
+#  }
+
