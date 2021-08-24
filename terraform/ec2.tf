5229cbb0312b8bece2240794a48b2506eaec62a5
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-24 19:53:20 +0530

    Ansible

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,11 +18,8 @@ resource "aws_spot_instance_request" "cheap_worker" {
 resource "aws_instance" "app-instances" {
   count                       = length(var.APP_COMPONENTS)
   ami                         = "ami-074df373d6bafa625"
-  spot_price                  = "0.0031"
   instance_type               = "t3.micro"
   vpc_security_group_ids      = ["sg-083a944fa2575b4b6"]
-  wait_for_fulfillment        = true
-  spot_type                   = "persistent"
   tags                        = {
     Name                      = "${element(var.APP_COMPONENTS, count.index)}-${var.ENV}"
     Monitor                   = "yes"
